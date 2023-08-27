c481ad172d74a50778efa9b6613a31045e0b6a13
Mike <102953584+michelangelodorado@users.noreply.github.com>
2023-08-27 22:25:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 #based on https://docs.nginx.com/nginx-management-suite/installation/vm-bare-metal/prerequisites/
 #created Aug 2023
+
+#Install NGINX Plus
+sudo apt-get install -y apt-transport-https lsb-release ca-certificates wget gnupg2 ubuntu-keyring
+wget -qO - https://cs.nginx.com/static/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+wget -qO - https://cs.nginx.com/static/keys/app-protect-security-updates.key | gpg --dearmor | sudo tee /usr/share/keyrings/app-protect-security-updates.gpg >/dev/null
+printf "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://pkgs.nginx.com/plus/ubuntu `lsb_release -cs` nginx-plus\n" | sudo tee /etc/apt/sources.list.d/nginx-plus.list
+sudo wget -P /etc/apt/apt.conf.d https://cs.nginx.com/static/files/90pkgs-nginx
+sudo apt-get update
+sudo apt-get install -y nginx-plus
+
 #Download Certificate and Key
 sudo mkdir -p /etc/ssl/nginx
 sudo mv <nginx-mgmt-suite-trial.crt> /etc/ssl/nginx/nginx-repo.crt
 sudo mv <nginx-mgmt-suite-trial.key> /etc/ssl/nginx/nginx-repo.key
 
 #Install ClickHouse
+sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates dirmngr
 GNUPGHOME=$(mktemp -d)
 sudo GNUPGHOME="$GNUPGHOME" gpg --no-default-keyring --keyring /usr/share/keyrings/clickhouse-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8919F6BD2B48D754
@@ -18,7 +29,7 @@ sudo apt-get update
 sudo apt-get install -y clickhouse-server clickhouse-client
 sudo systemctl enable clickhouse-server
 sudo systemctl start clickhouse-server
-sudo systemctl status clickhouse-server
+#sudo systemctl status clickhouse-server
 
 #Add NGINX Management Suite Repository
 printf "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://pkgs.nginx.com/nms/ubuntu `lsb_release -cs` nginx-plus\n" | sudo tee /etc/apt/sources.list.d/nms.list
